1b1d70628d66b86ce070ac74fdfe0004616b97ca
David <alv.ricardodavid@gmail.com>
2024-10-07 10:28:11 -0300

    Encriptando la contraseña

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,12 +4,13 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from "bcryptjs";
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-
 import { User } from './entities/user.entity';
-import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
@@ -17,8 +18,14 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     try {
-      const newUser = new this.userModel(createUserDto);
-      return await newUser.save();
+      const { password, ...userData } = createUserDto
+      
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync( password, 10),
+        ...userData
+      });
+
+       return await newUser.save();
     } catch (error) {
       console.log(error.code);
       if (error.code === 11000) {
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -11,7 +11,7 @@ export class User {
  name: string;
 
  @Prop({ minlength: 6, required: true })
- password: string;
+ password?: string;
 
  @Prop({ default: true })
  isActive: boolean;
